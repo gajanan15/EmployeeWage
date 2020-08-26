1350898405fa9e540dbf89f73555fe471858037a
gajanan15 <gajanandesai97@gmail.com>
2020-08-26 20:16:59 +0530

    [Gajanan]ADDED : Code For Employee Attendance

--- EmployeeWage/app.js
+++ EmployeeWage/app.js
@@ -1,2 +1,7 @@
-console.log('Hello world');
+console.log('Welcome To Employee Wage Problem');
+//CONSTANT VALUE
+const EMPLOYEE_IS_PRESENT = 1;
+let employeeAttendance = Math.ceil(Math.random()) % 2;
+console.log(employeeAttendance);
+employeeAttendance === EMPLOYEE_IS_PRESENT ? console.log("Employee Present") : console.log("Employee Absent");
 //# sourceMappingURL=app.js.map
--- EmployeeWage/app.ts
+++ EmployeeWage/app.ts
@@ -1,1 +1,9 @@
 console.log('Welcome To Employee Wage Problem');
+
+//CONSTANT VALUE
+const EMPLOYEE_IS_PRESENT = 1;
+
+let employeeAttendance = Math.ceil(Math.random()) % 2;
+console.log(employeeAttendance);
+
+employeeAttendance === EMPLOYEE_IS_PRESENT ? console.log("Employee Present") : console.log("Employee Absent");

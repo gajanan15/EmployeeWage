97d37ebb0b8ec959f2ed0764064d78c543a50563
gajanan15 <gajanandesai97@gmail.com>
2020-08-26 20:59:24 +0530

    [Gajanan]ADDED : Wage Calculation Of Part Time Employee

--- EmployeeWage/app.ts
+++ EmployeeWage/app.ts
@@ -1,17 +1,25 @@
 console.log('Welcome To Employee Wage Problem');
 
 //CONSTANT VALUE
-const EMPLOYEE_IS_PRESENT = 1;
-var EMPLOYEE_RATE_PER_HR = 20;
-var EMPLOYEE_HRS = 8;
+const WORKING_PART_TIME = 0;
+const WORKING_FULL_TIME = 1;
+const EMPLOYEE_RATE_PER_HR = 20;
+
+//Variable
+var employeeHrs;
 var salary;
 
-let employeeAttendance = Math.ceil(Math.random()) % 2;
+let employeeAttendance = Math.round(Math.random()) % 2;
 
-if (EMPLOYEE_IS_PRESENT == employeeAttendance) {
-    salary = (EMPLOYEE_HRS * EMPLOYEE_RATE_PER_HR);
-    console.log("Employee Daily Wage : " + salary);
+if (WORKING_FULL_TIME == employeeAttendance) {
+    employeeHrs = 8;
+}
+else if (WORKING_PART_TIME == employeeAttendance) {
+    employeeHrs = 4;
 }
 else {
-    salary = 0;
+    employeeHrs = 0;
 }
+
+salary = (employeeHrs * EMPLOYEE_RATE_PER_HR)
+console.log("Employee Daily Wage : " + salary);

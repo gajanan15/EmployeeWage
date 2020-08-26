e18d665d6af1eb55f65376ef51fb731707f0d1ba
gajanan15 <gajanandesai97@gmail.com>
2020-08-26 20:32:47 +0530

    [Gajanan]ADDED : Calculating Daily Employee Wage

--- EmployeeWage/app.ts
+++ EmployeeWage/app.ts
@@ -2,8 +2,16 @@ console.log('Welcome To Employee Wage Problem');
 
 //CONSTANT VALUE
 const EMPLOYEE_IS_PRESENT = 1;
+var EMPLOYEE_RATE_PER_HR = 20;
+var EMPLOYEE_HRS = 8;
+var salary;
 
 let employeeAttendance = Math.ceil(Math.random()) % 2;
-console.log(employeeAttendance);
 
-employeeAttendance === EMPLOYEE_IS_PRESENT ? console.log("Employee Present") : console.log("Employee Absent");
+if (EMPLOYEE_IS_PRESENT == employeeAttendance) {
+    salary = (EMPLOYEE_HRS * EMPLOYEE_RATE_PER_HR);
+    console.log("Employee Daily Wage : " + salary);
+}
+else {
+    salary = 0;
+}

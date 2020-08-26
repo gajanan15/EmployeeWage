59abf4ccb4aba952b3ab8968bd1f2cd85d23e765
gajanan15 <gajanandesai97@gmail.com>
2020-08-26 22:17:12 +0530

    [Gajanan]ADDED : Calculate Wages For Month

--- EmployeeWage/app.js
+++ EmployeeWage/app.js
@@ -3,23 +3,25 @@ console.log('Welcome To Employee Wage Problem');
 const WORKING_PART_TIME = 1;
 const WORKING_FULL_TIME = 2;
 const EMPLOYEE_RATE_PER_HR = 20;
+const NUMBER_OF_WORKING_DAYS = 20;
 //Variable
 var employeeHrs;
 var salary;
-let employeeAttendance = Math.round(Math.random() * 10) % 3;
-switch (employeeAttendance) {
-    case WORKING_FULL_TIME:
-        console.log("Empoyee Working Full Time");
-        employeeHrs = 8;
-        break;
-    case WORKING_PART_TIME:
-        console.log("Empoyee Working Part Time");
-        employeeHrs = 4;
-        break;
-    default:
-        console.log("Empoyee Absent");
-        employeeHrs = 0;
+var totalSalary = 0;
+for (let day = 1; day <= NUMBER_OF_WORKING_DAYS; day++) {
+    let employeeAttendance = Math.round(Math.random() * 10) % 3;
+    switch (employeeAttendance) {
+        case WORKING_FULL_TIME:
+            employeeHrs = 8;
+            break;
+        case WORKING_PART_TIME:
+            employeeHrs = 4;
+            break;
+        default:
+            employeeHrs = 0;
+    }
+    salary = (employeeHrs * EMPLOYEE_RATE_PER_HR);
+    totalSalary += salary;
 }
-salary = (employeeHrs * EMPLOYEE_RATE_PER_HR);
-console.log("Employee Daily Wage : " + salary);
+console.log("Wages for a Month :  " + totalSalary);
 //# sourceMappingURL=app.js.map
